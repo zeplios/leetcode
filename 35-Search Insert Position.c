525c45c85de401997eb715d4ddb8f3d566f744bd
zeplios <zhfchdev@gmail.com>
2015-08-22 14:34:21 +0800

    rename

--- 35-Search Insert Position.c
+++
@@ -1,9 +0,0 @@
-int searchInsert(int* nums, int numsSize, int target) {
-    int i;
-    for (i = 0 ; i < numsSize ; i++) {
-        if (target <= nums[i]) {
-            return i;
-        }
-    }
-    return numsSize;
-}
--- 53-Maximum Subarray.c
+++
@@ -1,9 +0,0 @@
-int maxSubArray(int* nums, int numsSize) {
-    int lastmax = nums[0], newmax = 0, i;
-    for (i = 0 ; i < numsSize ; i++) {
-        newmax += nums[i];
-        lastmax = newmax > lastmax ? newmax : lastmax;
-        newmax = newmax > 0 ? newmax : 0;
-    }
-    return lastmax;
-}
