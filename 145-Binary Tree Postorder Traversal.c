538b5b7050fba806d0aa2f3a91eaf734e5d1a748
Fucheng Zhang <zhfchdev@gmail.com>
2016-02-06 10:52:57 +0800

    Update 145-Binary Tree Postorder Traversal.c

--- 145-Binary Tree Postorder Traversal.c
+++ 145-Binary Tree Postorder Traversal.c
@@ -11,35 +11,27 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* postorderTraversal(struct TreeNode* root, int* returnSize) {
-    struct TreeNode** stack = malloc(100 * sizeof(struct TreeNode*));
-    int* stack4LeftOver = malloc(100 * sizeof(int));
     int * res = malloc(1000 * sizeof(int));
-    int stackTop = 0;
-    (*returnSize) = 0;
-    struct TreeNode* cur = root;
-    while (cur != NULL) {
-        stack[stackTop] = cur;
-        stack4LeftOver[stackTop++] = 0;
-        if (cur->left != NULL) {
+    struct TreeNode** stack = malloc(100 * sizeof(struct TreeNode*));
+    int top = 0, count = 0;
+    struct TreeNode* cur = root, *prePop = NULL;
+    
+    while (top > 0 || cur != NULL) {
+        while (cur != NULL) {
+            stack[top++] = cur;
             cur = cur->left;
+        }
+        
+        cur = stack[top-1];
+        if (cur->right != NULL && prePop != cur->right) {
+            cur = cur->right;
         } else {
-            while (true) {
-                if (stackTop == 0) {
-            		cur = NULL;
-            		break;
-				}
-                if (stack4LeftOver[stackTop-1]) {
-                    res[(*returnSize)++] = stack[--stackTop]->val;
-                } else {
-                    cur = stack[stackTop-1];
-                    stack4LeftOver[stackTop-1] = 1;
-                    if (cur->right != NULL) {
-                        cur = cur->right;
-                        break;
-                    }
-                }
-            }
+            res[count++] = cur->val;
+            top--;
+            prePop = cur;
+            cur = NULL;
         }
     }
+    (*returnSize) = count;
     return res;
 }
