89927db749b71660c60a84c94773b660ef230979
Fucheng Zhang <zhfchdev@gmail.com>
2016-02-08 21:51:43 +0800

    Update 327-Count of Range Sum.c

--- 327-Count of Range Sum.c
+++ 327-Count of Range Sum.c
@@ -15,13 +15,13 @@ typedef struct HashTable {
 } HashTable;
 
 HashTable* hashtable_create(int size) {
-	if (size < 1) size = 1;
-	HashTable * table = malloc(sizeof(HashTable));
-	table->bucketSize = size;
-	table->eleCount = 0;
-	table->buckets = malloc(size * sizeof(HashTable *));
-	memset(table->buckets, 0, size * sizeof(HashTable *));
-	return table;
+    if (size < 1) size = 1;
+    HashTable * table = malloc(sizeof(HashTable));
+    table->bucketSize = size;
+    table->eleCount = 0;
+    table->buckets = malloc(size * sizeof(HashTable *));
+    memset(table->buckets, 0, size * sizeof(HashTable *));
+    return table;
 }
 
 // use HashNode instand of int for NULL returnt
@@ -43,35 +43,35 @@ void hashtable_set(HashTable* table, K key, V value) {
     while (hash < 0) hash += table->bucketSize;
     HashNode* node = table->buckets[hash], *prenode = NULL;
     while (node != NULL && node->key != key) {
-    	prenode = node;
-    	node = node->next;
-	}
-	
-	if (node != NULL) {
-		node->value = value;
-	} else {
-		HashNode * tmp = malloc(sizeof(HashNode));
-    	tmp->key = key;
-    	tmp->value = value;
-    	tmp->next = NULL;
-    	if (prenode != NULL) {
-    		prenode->next = tmp;
-		} else {
-			table->buckets[hash] = tmp;
-		}
-		table->eleCount++;
-	}
+        prenode = node;
+        node = node->next;
+    }
+
+    if (node != NULL) {
+        node->value = value;
+    } else {
+        HashNode * tmp = malloc(sizeof(HashNode));
+        tmp->key = key;
+        tmp->value = value;
+        tmp->next = NULL;
+        if (prenode != NULL) {
+            prenode->next = tmp;
+        } else {
+            table->buckets[hash] = tmp;
+        }
+        table->eleCount++;
+    }
 }
 HashNode** hashtable_iter(HashTable* table) {
-	HashNode* nodes[table->eleCount];
-	int i, c = 0;
-	for (i = 0 ; i < table->bucketSize ; i++) {
-		HashNode* n = table->buckets[i];
-		while (n != NULL) {
-			nodes[c++] = n;
-			n = n->next;
-		}
-	}
+    HashNode* nodes[table->eleCount];
+    int i, c = 0;
+    for (i = 0 ; i < table->bucketSize ; i++) {
+        HashNode* n = table->buckets[i];
+        while (n != NULL) {
+            nodes[c++] = n;
+            n = n->next;
+        }
+    }
     return nodes;
 }
 // ----------------------an implentation of HashTable------------------
@@ -82,20 +82,20 @@ int countRangeSum(int* nums, int numsSize, int lower, int upper) {
     long i, j, sum = 0, count = 0, diff = upper - lower + 1;
     hashtable_set(table, 0, 1);
     for (i = 0 ; i < numsSize ; i++) {
-    	sum += nums[i];
-    	if (diff < table->eleCount) {
-    		for (j = sum - upper ; j <= sum - lower ; j++) {
-	    		count += hashtable_get_def(table, j, 0);
-			}
-		} else {
-			HashNode** nodes = hashtable_iter(table);
-			for (j = 0 ; j < table->eleCount ; j++) {
-				if (sum - nodes[j]->key <= upper && sum - nodes[j]->key >= lower) {
-					count += nodes[j]->value;
-				}
-			}
-		}
-    	hashtable_set(table, sum, hashtable_get_def(table, sum, 0)+1);
-	}
-	return count;
+        sum += nums[i];
+        if (diff < table->eleCount) {
+            for (j = sum - upper ; j <= sum - lower ; j++) {
+                count += hashtable_get_def(table, j, 0);
+            }
+        } else {
+            HashNode** nodes = hashtable_iter(table);
+            for (j = 0 ; j < table->eleCount ; j++) {
+                if (sum - nodes[j]->key <= upper && sum - nodes[j]->key >= lower) {
+                    count += nodes[j]->value;
+                }
+            }
+        }
+        hashtable_set(table, sum, hashtable_get_def(table, sum, 0)+1);
+    }
+    return count;
 }
